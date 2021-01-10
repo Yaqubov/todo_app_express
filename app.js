985fedf01d08d13c8cbf04f84c09b15f8aa6321a
Tahir Yagubov <tahiryaqubov87@gmail.com>
2021-01-10 21:48:31 +0400

    4th commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const date = require('date-and-time');
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+const { check, validationResult } = require('express-validator')
 
 const ACCESS_TOKEN_SECRET = 'pr!v@te_key'
 
@@ -48,49 +49,83 @@ app.get('/',authenticateToken, (req,res) => {
     })               
 })
 
-app.post('/',(req,res) => {
-    const username = req.body.username
-    const password = req.body.password
-    db.query("SELECT * FROM users WHERE username='"+username+"'",(err,result) => {
-        if (err) throw err;
-        if (!result.length) {
-            res.send('User Not Found')
-        }else{
-            if (bcrypt.compareSync(password,result[0].password)){
-                const user = { 'id':result[0].id, 'username':result[0].username, 'password':result[0].password }
-                const token = jwt.sign(user, ACCESS_TOKEN_SECRET)
-                res.cookie('token', token)
-                db.query("SELECT * FROM todos WHERE userId='"+user.id+"'",(err,result)=>{
-                    if (err) throw err;
-                    res.render('main',{ userID:user.id, todos:result })
-                })
+app.post('/',
+    [check('username','Fill username').notEmpty(),
+     check('password','Fill password').notEmpty()],
+    (req,res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        const alert = errors.array()
+        res.render('login',{ errors: alert })
+    }else {
+        const username = req.body.username
+        const password = req.body.password
+        
+        db.query("SELECT * FROM users WHERE username='"+username+"'",(err,result) => {
+            if (err) throw err;
+            if (!result.length) {
+                res.render('login',{ errors: [{msg: "User not found"}] })
             }else{
-                res.send('Wrong Password')
-            }   
-        }
-    })
+                if (bcrypt.compareSync(password,result[0].password)){
+                    const user = { 'id':result[0].id, 'username':result[0].username, 'password':result[0].password }
+                    const token = jwt.sign(user, ACCESS_TOKEN_SECRET)
+                    res.cookie('token', token)
+                    db.query("SELECT * FROM todos WHERE userId='"+user.id+"'",(err,result)=>{
+                        if (err) throw err;
+                        res.render('main',{ userID:user.id, todos:result })
+                    })
+                }else{
+                    res.render('login',{ errors: [{msg: "Wrong password"}] })
+                }   
+            }
+        }) 
+    }
 })
 
 app.get('/register',(req,res)=>{
     res.render('register')
 })
 
-app.post('/register',(req,res)=>{
-    const username = req.body.username
-    const password = req.body.password
-    const password_hash = bcrypt.hashSync(password, 10)
-    db.query("INSERT INTO users(username,password) VALUES ('"+username+"','"+password_hash+"')",(err,result)=>{
-        if(err) throw err;
-        res.render('result',{message: 'You succesfully registered!'})
-    })
+app.post('/register',[
+    check('username','Fill username').notEmpty(),
+    check('password','Password must be longer than 6 characters').exists().isLength({ min:6 })],
+    (req,res)=>{
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        const alert = errors.array()
+        res.render('register',{ errors: alert })
+    }else{
+        const username = req.body.username
+        db.query("SELECT username FROM users WHERE username='"+username+"'",(err,result)=>{
+            if (result.length){
+                res.render('register',{ errors: [{msg:'Username is already in use'}] })
+            }else{
+                const password = req.body.password
+                const password_hash = bcrypt.hashSync(password, 10)
+                db.query("INSERT INTO users(username,password) VALUES ('"+username+"','"+password_hash+"')",(err,result)=>{
+                    if(err) throw err;
+                    res.render('result',{message: 'You succesfully registered!'})
+                })
+            }
+        })
+    }
 })
 
-app.post('/add', authenticateToken, (req,res)=> {
+app.post('/add', authenticateToken, [
+    check('name',"Fill To Do name").notEmpty()], 
+    (req,res)=> {
     const user = req.user
-    const now = new Date();
-    const pattern = date.compile('ddd, MMM DD YYYY HH:mm:ss');
-    const todoname = req.body.name
-    if (todoname){
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        const alert = errors.array()
+        db.query("SELECT * FROM todos WHERE userId='"+user.id+"'",(err,result)=>{
+            if (err) throw err;
+            res.render('main',{ userID:user.id, todos:result, error:alert[0]})
+        })
+    }else{
+        const now = new Date();
+        const pattern = date.compile('ddd, MMM DD YYYY HH:mm:ss');
+        const todoname = req.body.name
         const sqlquery = "INSERT INTO todos(name,date,userId) VALUES ('"+todoname+"','"+date.format(now,pattern)+"','"+req.body.userid+"')"
         db.query(sqlquery, (err,result)=> {
             if (err) throw err;
@@ -98,9 +133,7 @@ app.post('/add', authenticateToken, (req,res)=> {
         db.query("SELECT * FROM todos WHERE userId='"+user.id+"'",(err,result)=>{
             if (err) throw err;
             res.render('main',{ userID:user.id, todos:result })
-        })  
-    }else{
-        res.render('result',{message: "To Do name can not be empty"})
+        })     
     }
 })
 
@@ -108,17 +141,25 @@ app.get('/edit/:id', authenticateToken, (req,res) => {
     res.render('edit', {id: req.params.id})
 })
 
-app.post('/edit/:id', authenticateToken, (req,res) => {
+app.post('/edit/:id', authenticateToken, [
+    check('name',"Fill To Do name for updating").notEmpty()], 
+    (req,res) => {
     const user = req.user
-    const now = new Date();
-    const pattern = date.compile('ddd, MMM DD YYYY HH:mm:ss');
-    db.query("UPDATE todos SET name='"+req.body.name+"' , date='"+date.format(now,pattern)+"' WHERE id='"+req.params.id+"'", (err,result) => {
-        if (err) throw err;
-    })
-    db.query("SELECT * FROM todos WHERE userId='"+user.id+"'",(err,result)=>{
-        if (err) throw err;
-        res.render('main',{ userID:user.id, todos:result })
-    })  
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        const alert = errors.array()
+        res.render('edit',{id: req.params.id, error: alert[0] })
+    }else{
+        const now = new Date();
+        const pattern = date.compile('ddd, MMM DD YYYY HH:mm:ss');
+        db.query("UPDATE todos SET name='"+req.body.name+"' , date='"+date.format(now,pattern)+"' WHERE id='"+req.params.id+"'", (err,result) => {
+            if (err) throw err;
+        })
+        db.query("SELECT * FROM todos WHERE userId='"+user.id+"'",(err,result)=>{
+            if (err) throw err;
+            res.render('main',{ userID:user.id, todos:result })
+        })
+    }
 })
 
 app.get('/delete/:id', authenticateToken, (req,res) => {
